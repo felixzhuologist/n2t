20e8a7039abd7cf5d4ac44f9ea8a270dc2aa7c5b
felixzhuologist <felix.czhu@gmail.com>
2017-12-04 09:44:12 -0800

    add file test for parsing

--- 10/Tests.hs
+++ 10/Tests.hs
@@ -16,7 +16,8 @@ main = do
   _ <- runTestTT (TestList [
         tParseFactor, tParseExpr,
         tParseStmt, tParseBlock,
-        tAttrs, tMethods])
+        tAttrs, tMethods,
+        tParseFile])
   return ()
 
 testp :: Parser a -> String -> Either ParseError a
@@ -59,8 +60,8 @@ tParseBlock = "pares block" ~: TestList [
 
 -- program structure parsing tests
 tAttrs = "parse attributes" ~: TestList [
-          "static int a, b, c;" ~: testp attr "static int a, b, c;" ~?= Right (Static, JInt, (fromJust $ nonEmpty ["a", "b", "c"])),
-          "field Dog my_dog;" ~: testp attr "field Dog my_dog;" ~?= Right (Field, JObject, (fromJust $ nonEmpty ["my_dog"]))]
+          "static int a, b, c;" ~: testp attr "static int a, b, c;" ~?= Right (Static, JInt, (fromList ["a", "b", "c"])),
+          "field Dog my_dog;" ~: testp attr "field Dog my_dog;" ~?= Right (Field, JObject, (fromList ["my_dog"]))]
 
 add_method :: MethodDecl
 add_method
@@ -68,6 +69,57 @@ add_method
     Void,
     "add",
     [(JInt, "a"), (JInt, "b")],
-    ([(JInt, fromJust $ nonEmpty ["c"])], [Return]))
+    ([(JInt, fromList ["c"])], [Return]))
 tMethods = "parse method defs" ~: TestList [
               "method" ~: testp method "constructor void add(int a, int b) {var int c; return;}" ~?= Right add_method]
+
+-- e2e test
+squareGame :: Program
+squareGame =
+  Class
+    "SquareGame"
+    [(Field, JObject, fromList ["square"]),
+     (Field, JInt, fromList ["direction"])]
+    [(ConstructorDecl, JObject , "new", [], (
+        [],
+        [Let "square" (Call (Method "Square" "new" [IntVal 0,IntVal 0,IntVal 30])),
+         Let "direction" (IntVal 0),ReturnVal This])),
+     (MethodDecl, Void, "dispose", [], (
+        [],
+        [Do (Method "square" "dispose" []),
+         Do (Method "Memory" "deAlloc" [This]),
+         Return])),
+     (MethodDecl, Void, "moveSquare", [], (
+        [],
+        [If (Binary Eq (Var "direction") (IntVal 1)) [Do (Method "square" "moveUp" [])],
+         If (Binary Eq (Var "direction") (IntVal 2)) [Do (Method "square" "moveDown" [])],
+         If (Binary Eq (Var "direction") (IntVal 3)) [Do (Method "square" "moveLeft" [])],
+         If (Binary Eq (Var "direction") (IntVal 4)) [Do (Method "square" "moveRight" [])],
+         Do (Method "Sys" "wait" [IntVal 5]),Return])),
+     (MethodDecl, Void, "run", [], (
+      [(JChar,"key" :| []),
+       (JBool,"exit" :| [])],
+      [Let "exit" (BoolVal False),
+       While (Unary Not (Var "exit"))
+        [While (Binary Eq (Var "key") (IntVal 0))
+          [Let "key" (Call (Method "Keyboard" "keyPressed" [])),
+           Do (Func "moveSquare" [])],
+           If (Binary Eq (Var "key") (IntVal 81)) [Let "exit" (BoolVal True)],
+           If (Binary Eq (Var "key") (IntVal 90)) [Do (Method "square" "decSize" [])],
+           If (Binary Eq (Var "key") (IntVal 88)) [Do (Method "square" "incSize" [])],
+           If (Binary Eq (Var "key") (IntVal 131)) [Let "direction" (IntVal 1)],
+           If (Binary Eq (Var "key") (IntVal 133)) [Let "direction" (IntVal 2)],
+           If (Binary Eq (Var "key") (IntVal 130)) [Let "direction" (IntVal 3)],
+           If (Binary Eq (Var "key") (IntVal 132)) [Let "direction" (IntVal 4)],
+           While (Unary Not (Binary Eq (Var "key") (IntVal 0)))
+            [Let "key" (Call (Method "Keyboard" "keyPressed" [])),
+             Do (Func "moveSquare" [])]],
+       Return]))]
+
+tParseFile :: Test
+tParseFile = "parse file" ~: TestList ["SquareGame.jack" ~: p "Square/SquareGame.jack" squareGame] where
+  p file ast = do
+    result <- parseFromFile program file
+    case result of
+      (Left _) -> assert False
+      (Right ast') -> assert (ast == ast')
